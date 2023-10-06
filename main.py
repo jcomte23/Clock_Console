5ac197fbe6d3f8c5fd432aae4fe2f368657c3a2c
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-06 13:24:32 -0500

    mejora el formato y carga de exepcion

--- main.py
+++ main.py
@@ -7,15 +7,22 @@ print("|   Clock Console   |")
 print("|___________________|")
 print()
 
-hora=int(input("In 24 hour format what time of day is it? =>"))
-minuto=int(input("What minute of the day is it? =>"))
+try:
+    hora=int(input("In 24 hour format what time of day is it? =>"))
+    minuto=int(input("What minute of the day is it? =>"))
 
-while True:
-    for horas in range(hora,24):
-        for minutos in range(minuto,60):
-            for segundos in range(60):
-                time.sleep(1)
-                os.system("cls")
-                print(f"Hora: {horas:02d}:{minutos:02d}:{segundos:02d}")
-        minuto=0
-    hora=0
+    if hora < 0 or hora > 23 or minuto < 0 or minuto > 59:
+        print("Hour or minute out of range.")
+        exit()
+
+    while True:
+        for horas in range(hora,24):
+            for minutos in range(minuto,60):
+                for segundos in range(60):
+                    time.sleep(1)
+                    os.system("cls")
+                    print(f"Hora: {horas:02d}:{minutos:02d}:{segundos:02d}")
+            minuto=0
+        hora=0
+except:
+    print("Invalid hour or minute entered.")
