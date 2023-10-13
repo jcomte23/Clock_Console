202ce86fe6aacfdea7a8a65824d5d3dbef4b255b
Javier Combita Tellez <javiercombita2014@gmail.com>
2023-10-13 11:51:12 -0500

    mejoras en el la estetica del codigo

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ try:
     if hour < 0 or hour > 23 or minutes < 0 or minutes > 59:
         print(" _________________________________________________")
         print("|                                                 |")
-        print("|    / \\                                         |")
-        print("|   / | \\     Hour or minute out of range.       |")
-        print("|  /  *  \\                                       |")
-        print("| /_______\\                                      |")
+        print("|    / \\                                          |")
+        print("|   / | \\     Hour or minute out of range.        |")
+        print("|  /  *  \\                                        |")
+        print("| /_______\\                                       |")
         print("|_________________________________________________|")
         print("")
         exit()
@@ -28,15 +28,19 @@ try:
             for minutes in range(minutes, 60):
                 for seconds in range(60):
                     time.sleep(1)
-                    os.system("cls")
-                    print(f"Hora: {horas: 02d}={minutes: 02d}={seconds: 02d}")
+                    os.system("clear")
+                    print(f"Hora=> {horas:02d}:{minutes:02d}:{seconds:02d}")
             minutes = 0
         hour = 0
-except ValueError:
+except ValueError as ve:
     print(" _________________________________________________")
     print("|                                                 |")
-    print("|    / \\                                         |")
-    print("|   / | \\    Invalid hour or minute entered.     |")
-    print("|  /  *  \\                                       |")
-    print("| /_______\\                                      |")
+    print("|    / \\                                          |")
+    print("|   / | \\    Invalid hour or minute entered.      |")
+    print("|  /  *  \\                                        |")
+    print("| /_______\\                                       |")
     print("|_________________________________________________|")
+    print()
+    print(f"ERROR => {ve}")
+    print()
+
